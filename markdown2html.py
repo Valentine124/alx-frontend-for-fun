946ea1a5e84768c3d55b2379344aa417556fb48f
Valentine124 <kaluvalentine60@gmail.com>
2024-10-19 17:19:44 +0100

    Task 0

---
+++ markdown2html.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script for HTML markdown"""
+
+
+if __name__ == '__main__':
+    from sys import argv, exit, stderr
+    from os.path import exists
+
+    if len(argv) < 2:
+        print(f'Usage: {argv[0]} README.md README.html', file=stderr)
+        exit(1)
+    if not(exists(argv[1])):
+        print(f'Missing {argv[1]}')
+        exit(1)
+    exit(0)
